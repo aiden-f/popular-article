ea1863eaf216409967f0093b099bf8a0700e79be
sijoo82 <sijoo82@efil.io>
2026-04-14 07:23:59 +0900

    구글 애드센스 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ko" className="light h-full" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-2723326980364617" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2723326980364617"
           crossOrigin="anonymous"></script>
       </head>
