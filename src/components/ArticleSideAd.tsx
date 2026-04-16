59cd9b2c80b54276f5e373b5546114bfd4c6022d
sijoo82 <sijoo82@efil.io>
2026-04-16 09:43:20 +0900

    로켓 배송 추가

--- src/components/ArticleSideAd.tsx
+++ src/components/ArticleSideAd.tsx
@@ -14,7 +14,7 @@ const VerticlaSearchCoupangBanner = dynamic(
 export function ArticleSideAd() {
   return (
     <aside className="hidden xl:flex flex-col gap-4 pt-10 pr-6 w-[360px] shrink-0">
-      <div className="sticky top-8 flex flex-col gap-4">
+      <div className=" flex flex-col gap-4">
         {/* 광고 라벨 */}
         <div className="flex items-center gap-2">
           <div className="h-px flex-1 bg-slate-100" />
